f50b7d9372454ac07322ffbaa6574777b3519b97
Marcel <marcelkaemper862@gmail.com>
2019-04-06 11:04:43 +0200

    Added scalars

---
+++ scalars.pl
@@ -0,0 +1,23 @@
+# Basic scalars
+$age = 25;
+$name = "Marcel";
+$salary = 14244.23;
+
+# Numeric scalars
+$integer = 200;
+$negative = -300;
+$floating = 234.234;
+
+# Scalar operations
+$str = "Hello" . "World";
+$num = 24 + 25;
+$mul = 5 * 5;
+$mix = $str.$num;
+
+# V-Strings
+$smile = v9786;
+print("$smile\n");
+
+# Special characters
+print(__LINE__." ".__FILE__." ".__PACKAGE__."\n");
+
